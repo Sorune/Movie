ffbe1a3467c083ec955dd43c1a232c33bba8ad83
Sorune <76832007+Sorune@users.noreply.github.com>
2024-03-26 10:01:37 +0900

    actor register  등록 로직 추가

--- Movie/src/main/webapp/resources/js/common.js
+++ Movie/src/main/webapp/resources/js/common.js
@@ -67,25 +67,25 @@ $('.fa-star').on("click", function() {             // 별 아이콘을 누르면
 });
 
 // file upload drag 
-const dropArea = document.getElementById("uploadedImages");
-var fileInput = document.getElementById("file-input");
-const imagePreview = document.getElementById("image-preview");
-const dataTranster = new DataTransfer();
-var uploadedImages = document.getElementById("uploadedImages");
-var uploadImageButton = document.getElementById("uploadImage");
-var deleteImageButton = document.getElementById("deleteImage");
+const dropArea = document.getElementById("uploadedImages");		//이미지 드래그 드롭 필드 등록
+var fileInput = document.getElementById("file-input");			//input type="file" 태그
+const imagePreview = document.getElementById("image-preview");	
+const dataTranster = new DataTransfer();						//데이터 전송용 객체
+var uploadedImages = document.getElementById("uploadedImages");	//이미지 표시할 캐러셀
+var uploadImageButton = document.getElementById("uploadImage");	//캐러셀 이미지 등록 버튼
+var deleteImageButton = document.getElementById("deleteImage");	//캐러셀 이미지 삭제 버튼
 //var formObj = document.querySelector("form");
 
-if (!(uploadedImages == null)) {
-	uploadedImages = uploadedImages.querySelector("div");
+if (!(uploadedImages == null)) {							//캐러셀이 없을 경우
+	uploadedImages = uploadedImages.querySelector("div");	//이미지 드롭 필드 변경
 }
 const inputFile = $("input[name='uploadFile']");
 
-function onClickFunction() {
+function onClickFunction() {	//필드 클릭시 이미지 등록 이벤트 등록
 	fileInput.click();
 }
 
-function createUploadFileInput(form) {
+function createUploadFileInput(form) {  //폼을 받아 이미지 input태그를 폼에 삽입하는 메서드
 	let nodes = document.querySelector("#uploadedImages").querySelectorAll("img");
 	let inputString = "";
 	$(nodes).each(function(i, obj) {
@@ -106,19 +106,19 @@ function createUploadFileInput(form) {
 	form.append(inputString).submit();
 }
 
-function createCarouselInner(fileList) {
+function createCarouselInner(fileList) {	//업로드된 이미지를 캐러셀에 등록
 	let nodes = document.querySelector("#uploadedImages").querySelector("div").querySelectorAll("div");
 	for (var i = 0; i < nodes.length; i++) {
 		nodes[i].remove();
 	};
 	$(fileList).each(function(i, obj) {
-		const carouselInner = document.createElement("div");
+		const carouselInner = document.createElement("div");	//div 태그 생성
 		if (i == 0) {
 			carouselInner.className = "carousel-item active";
 		} else {
 			carouselInner.className = "carousel-item";
 		}
-		const img = document.createElement("img");
+		const img = document.createElement("img");				//img 태그 생성
 		var fileCallPath = encodeURIComponent(obj.uploadPath + "/" + obj.uuid + "_" + obj.fileName);
 		console.log(fileCallPath);
 		img.src = "/download?fileName=" + fileCallPath;
@@ -127,20 +127,20 @@ function createCarouselInner(fileList) {
 		img.setAttribute('class', "d-block w-100");
 		img.setAttribute('uploadPath', obj.uploadPath);
 		img.setAttribute('uuid', obj.uuid);
-		img.setAttribute('fileName', obj.fileName);
-		carouselInner.appendChild(img);
-		uploadedImages.appendChild(carouselInner);
+		img.setAttribute('fileName', obj.fileName);				//img태그에 데이터 등록
+		carouselInner.appendChild(img);							//img태그를 div에 등록
+		uploadedImages.appendChild(carouselInner);				//완성된 div를 캐러셀에 등록
 	});
 	
 	//createUploadFileInput(formObj);
 	console.log(uploadedImages);
-	dropArea.removeAttribute('eventList');
-	dropArea.removeEventListener("click", onClickFunction);
+	dropArea.removeAttribute('eventList');						//이벤트 등록 정보 태그 삭제
+	dropArea.removeEventListener("click", onClickFunction);		//캐러셀 클릭 이벤트 제거
 	console.log("event remove");
 
-	let nodeList = document.querySelector("#uploadedImages").querySelector("div").querySelectorAll("div");
+	let nodeList = document.querySelector("#uploadedImages").querySelector("div").querySelectorAll("div");	//캐러셀 이너 div 선택
 	console.log(nodeList);
-	if (nodeList.length == 0) {
+	if (nodeList.length == 0) {																				//캐러셀 이너 div에 내용이 없을 경우 기본 이미지 등록
 		var emptyNode = document.createElement("div");
 		var emptyP = document.createElement("p");
 		emptyNode.setAttribute("id", "drop-area");
@@ -149,8 +149,8 @@ function createCarouselInner(fileList) {
 		emptyNode.appendChild(emptyP);
 		document.querySelector("#uploadedImages").querySelector("div").appendChild(emptyNode);
 
-		dropArea.setAttribute('eventList', 'click');
-		dropArea.addEventListener("click", onClickFunction);
+		dropArea.setAttribute('eventList', 'click');														//클릭 이벤트 등록 정보 태그 생성
+		dropArea.addEventListener("click", onClickFunction);												//클릭 이벤트 등록
 		console.log("event set");
 	}
 
@@ -159,30 +159,30 @@ function createCarouselInner(fileList) {
 
 if (!(dropArea == null)) {
 	// 드래그 앤 드롭 이벤트 처리
-	dropArea.addEventListener("dragover", (e) => {
+	dropArea.addEventListener("dragover", (e) => {		//마우스 위치 이벤트 등록
 		e.preventDefault();
 		dropArea.style.backgroundColor = "#eee";
 	});
 
-	dropArea.addEventListener("dragleave", () => {
+	dropArea.addEventListener("dragleave", () => {		//마우스 위치 이벤트 등록
 		dropArea.style.backgroundColor = "#fff";
 	});
 
-	dropArea.addEventListener("drop", (e) => {
+	dropArea.addEventListener("drop", (e) => {			//이미지 드롭 이벤트 등록
 		const dataTranster = new DataTransfer();
 		const inputFile = document.querySelector("#file-input");
-		e.preventDefault();
+		e.preventDefault();								//기본 이벤트 정지, 정지하지 않을 경우 새 창에서 이미지 열림
 		dropArea.style.backgroundColor = "#fff";
-		const files = e.dataTransfer.files;
+		const files = e.dataTransfer.files;				//input태그에 담긴 파일들을 데이터 트랜스퍼 객체를 통해 옮겨담음
 		for (let i = 0; i < files.length; i++) {
 			let file = files[i];
 			if (file && file.type.startsWith("image")) {
-				dataTranster.items.add(file);
+				dataTranster.items.add(file);			//이미지일 경우 추가
 			}
 		};
-		inputFile.files = dataTranster.files;
+		inputFile.files = dataTranster.files;			//인풋태그의 파일들 교체
 		console.log(inputFile);
-		imgUpload(inputFile.files);
+		imgUpload(inputFile.files);						//서버로 이미지 데이터 전송
 	});
 
 
@@ -332,7 +332,7 @@ function imgUpload(files) {
 		type: 'POST',
 		success: function(result) {
 			console.log(result);
-			createCarouselInner(result);
+			createCarouselInner(result);			//이미지 업로드 성공시 업로드된 이미지를 보여줄 캐러셀 이미지 생성
 		},
 		error: function(result) {
 			alert("uploadFail");
@@ -340,7 +340,7 @@ function imgUpload(files) {
 		}
 	}); //$.ajax
 }
-// 이미지 표시 메서드
+// 이미지 표시 메서드(테스트용 코드)
 function displayImage(file) {
 	const reader = new FileReader();
 	reader.onload = () => {
@@ -363,16 +363,16 @@ function displayImage(file, caroucel) {
 }
 
 
-var formObj=$('form[role="form"]');
+var formObj=$('form[role="form"]');					//폼 선택 jquery
 
-$("button[type='submit']").on("click",function(e){
-	e.preventDefault();
+$("button[type='submit']").on("click",function(e){	//폼 등록버튼 선택
+	e.preventDefault();								//기본 동작 막기
 	console.log("submit clicked");
 	console.log(formObj);
 	//createUploadFileInput(formObj);
-	let nodes = document.querySelector("#uploadedImages").querySelectorAll("img");
+	let nodes = document.querySelector("#uploadedImages").querySelectorAll("img");	//캐러셀 이미지들 호출
 	let inputString = "";
-	$(nodes).each(function(i, obj) {
+	$(nodes).each(function(i, obj) {												//캐러셀 이미지들을 input태그에 담기
 		let inputFileName = document.createElement("input");
 		let inputFilePath = document.createElement("input");
 		let inputFileUuid = document.createElement("input");
@@ -388,23 +388,25 @@ $("button[type='submit']").on("click",function(e){
 		inputString += inputFileName.outerHTML+inputFilePath.outerHTML+inputFileUuid.outerHTML;
 	});
 	console.log(inputString);
-	formObj.append(inputString).submit();
-	var formnodes=document.querySelector("form").querySelectorAll("input");
+	formObj.append(inputString).submit();											//인풋태그들을 폼에 담아 전송(현재 동작 안함)
+	var formnodes=document.querySelector("form").querySelectorAll("input");			//폼의 인풋태그들 선택
 	console.log(formnodes);
-	var newForm = document.createElement("form");
+	var newForm = document.createElement("form");									//새 폼 생성
 	newForm.setAttribute("charset", "UTF-8");
 
  	newForm.setAttribute("method", "Post");  //Post 방식
 
  	newForm.setAttribute("action", "/actor/register"); //요청 보낼 주소
 
+	newForm.setAttribute("style","{display:none;}");
 
-	$(formnodes).each(function(i,obj){
+	$(formnodes).each(function(i,obj){												//옮겨온 input태그들을 새 폼에 등록
 		console.log(obj);
 		newForm.appendChild(formnodes[i]);
 	});
+	document.querySelector("body").appendChild(newForm);							//바디 태그에 폼 등록
 	console.log(newForm);
-	newForm.submit();
+	newForm.submit();																//폼 전송
 });
 
 // ajax file upload method
